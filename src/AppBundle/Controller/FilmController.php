ffacff3411eb4efb979b7bf93c80b5985504fcef
maxoon <desfouxmaxime@gmail.com>
2018-03-05 11:39:07 +0100

    add home page & route getAll

--- src/AppBundle/Controller/FilmController.php
+++ src/AppBundle/Controller/FilmController.php
@@ -2,12 +2,21 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Movie;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class FilmController extends Controller
 {
     public function homeAction()
     {
-        return $this->render('main.html.twig');
+        return $this->render('/film/home.html.twig');
+    }
+
+    public function getAllAction()
+    {
+        $repo = $this->getDoctrine()->getRepository(Movie::class);
+        $films = $repo->findAll();
+        var_dump($films);
+        die();
     }
 }
